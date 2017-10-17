ea0490b49f7f3a7a57a390ded5f0d78c9dc70ca9
Nicolas Valdiviezo <nvaldiviezon@gmail.com>
2017-10-17 19:38:50 -0400

    final version printing percentage of right answers

--- ID3_mushroom.py
+++ ID3_mushroom.py
@@ -184,7 +184,7 @@ def validator(filename, tree):
     features = raw_data.pop(0)
     features.remove("class")
     targets = [row.pop(0) for row in raw_data]
-    print(features)
+
     good = 0
     n = len(raw_data)
     row_indx = 0
@@ -220,12 +220,13 @@ def main():
     test_file = "test_dataset.csv"
 
     decision_tree = training(train_file)
-    death_toll = 1 - validator(test_file, decision_tree)
-
-    print(death_toll)
-
+    perc_correct = validator(test_file, decision_tree)
     print("============================")
     print_tree(decision_tree, 'root')
+    print("============================")
+    print("percent of correct classifications:", perc_correct)
+
+
 
 
 main()
